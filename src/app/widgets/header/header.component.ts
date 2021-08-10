b1ec0795f7b4a8319a4f239a57ea10705f1f2782
Razakason Jessi <razakasonjessi@gmail.com>
2021-08-10 14:24:08 +0300

    [profil][add]add profil

--- src/app/widgets/header/header.component.ts
+++ src/app/widgets/header/header.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Utilisateur } from 'src/app/models/utilisateur';
 import { LoggingService } from 'src/app/services/utilisateur/logging.service';
+import { Config } from 'src/app/utilitaire/config.model';
 
 @Component({
   selector: 'app-header',
@@ -9,9 +11,13 @@ import { LoggingService } from 'src/app/services/utilisateur/logging.service';
 })
 export class HeaderComponent implements OnInit {
   menuOpen = false ; 
+  usersession;
   constructor( private router:Router,private loggingService:LoggingService) { }
 
   ngOnInit(): void {
+    this.usersession = JSON.parse(localStorage.getItem(Config.getLocalStorageKey("user")));
+    console.log("session user : ",this.usersession);
+    console.log("session id : ",this.usersession._id);
   }
   toggleMenu(): void { 
     this.menuOpen = !this.menuOpen;
