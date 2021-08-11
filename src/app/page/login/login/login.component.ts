4c85b6fbbc5b216b17b4c4027b7ab4228c56e19f
Razakason Jessi <razakasonjessi@gmail.com>
2021-08-11 10:21:31 +0300

    [login][add]add spinner

--- src/app/page/login/login/login.component.ts
+++ src/app/page/login/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { NgxSpinnerService } from 'ngx-spinner';
 import { Utilisateur } from 'src/app/models/utilisateur';
 import { LoggingService } from 'src/app/services/utilisateur/logging.service';
 import { Config } from 'src/app/utilitaire/config.model';
@@ -14,24 +15,26 @@ export class LoginComponent implements OnInit {
   public username = null;
   public password = null;
   public error = null;
-  constructor(private router:Router, private loggingService:LoggingService) { }
+  constructor(private router:Router, private loggingService:LoggingService, private spinner: NgxSpinnerService) { }
 
   ngOnInit(): void {
   }
 
   onSubmit(event) {
     if((!this.username) || (!this.password)) return;
-
    let user = new Utilisateur();
    user.login = this.username;
    user.password = this.password;
    this.error=null;
+   this.spinner.show();
    this.loggingService.login(user)
    .subscribe(reponse => {
     localStorage.setItem(Config.getLocalStorageKey("user"),JSON.stringify(reponse));
+    this.spinner.hide();
     this.router.navigate(["/Pari"]);
   },err => {
     this.error = err.error.error;
+    this.spinner.hide();
   });
 }
 }
