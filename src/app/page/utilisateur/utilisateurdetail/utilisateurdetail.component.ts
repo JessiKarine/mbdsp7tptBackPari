1baf0c2e35bb1d3ee2172276b66b7285a93b7780
Laza Nomentsoa <starksedera@gmail.com>
2021-08-11 18:33:08 +0300

    modal init

--- src/app/page/utilisateur/utilisateurdetail/utilisateurdetail.component.ts
+++ src/app/page/utilisateur/utilisateurdetail/utilisateurdetail.component.ts
@@ -9,7 +9,7 @@ import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog
 export interface DialogData {
   description: string;
   titre: string ;
-  utilisateurData : Utilisateur;
+  utilisateurData : Object;
 }
 
 @Component({
@@ -20,15 +20,6 @@ export interface DialogData {
 export class UtilisateurdetailComponent implements OnInit {
   idUser : String;
   utilisateurSelected : Utilisateur;
-  login : String;
-  nom : String;
-  prenom : String;
-  email : String;
-  numeroTelephone : String;
-
-  animal: string;
-  name: string;
-  titre: String = "test titre";
 
   constructor(private route: ActivatedRoute,
                 private router: Router,
@@ -47,23 +38,12 @@ export class UtilisateurdetailComponent implements OnInit {
     this.utilisateurService.getUtilisateurByIdUser(idUser)
         .subscribe(utilisateur => {
             this.utilisateurSelected = utilisateur as Utilisateur;
-            this.login = this.utilisateurSelected.login;
-            this.nom = this.utilisateurSelected.nom;
-            this.prenom = this.utilisateurSelected.prenom;
-            this.email = this.utilisateurSelected.email;
-            this.numeroTelephone = this.utilisateurSelected.numeroTelephone;
             this.spinner.hide();
         })
   }
 
   onUpdate(){
     this.spinner.show();
-    console.log("login => " + this.login);
-    this.utilisateurSelected.login = this.login;
-    this.utilisateurSelected.nom = this.nom;
-    this.utilisateurSelected.prenom = this.prenom;
-    this.utilisateurSelected.email = this.email;
-    this.utilisateurSelected.numeroTelephone = this.numeroTelephone;
     this.utilisateurService.updateUtilisateurByIdUser(this.idUser, this.utilisateurSelected)
         .subscribe(utilisateur => {
             this.utilisateurSelected = utilisateur as Utilisateur
@@ -89,10 +69,6 @@ export class UtilisateurdetailComponent implements OnInit {
           data: {titre: "Delete", description: "Êtes-vous sur de modifier?", utilisateurData: this.utilisateurSelected}
         });
 
-        dialogRef.afterClosed().subscribe(result => {
-          console.log('The dialog was closed');
-          this.animal = result;
-        });
       }
 }
 
@@ -108,8 +84,10 @@ export class DialogOverviewExampleDialog {
   onNoClick(): void {
     this.dialogRef.close();
   }
+
+  // parametre number 0 = update et 1 = deleted;
   onAction() : void {
-          console.log("ok pour action => " + this.data.utilisateurData.prenom);
+          console.log("ok pour action => " + (this.data.utilisateurData as Utilisateur).prenom);
   }
 
 }
