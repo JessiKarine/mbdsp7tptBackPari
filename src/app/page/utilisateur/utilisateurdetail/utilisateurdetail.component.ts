c6c3ab1985ebec01580843c3e52c0f0c3447fdf1
Laza Nomentsoa <starksedera@gmail.com>
2021-08-11 20:40:02 +0300

    deleta utilisateur

--- src/app/page/utilisateur/utilisateurdetail/utilisateurdetail.component.ts
+++ src/app/page/utilisateur/utilisateurdetail/utilisateurdetail.component.ts
@@ -10,6 +10,8 @@ export interface DialogData {
   description: string;
   titre: string ;
   utilisateurData : Object;
+  typeOperation: Number;
+  idUser: string;
 }
 
 @Component({
@@ -20,12 +22,13 @@ export interface DialogData {
 export class UtilisateurdetailComponent implements OnInit {
   idUser : String;
   utilisateurSelected : Utilisateur;
+  titre:String;
+  description:String;
 
   constructor(private route: ActivatedRoute,
-                private router: Router,
-                    private utilisateurService: UtilisateurService,
-                    private spinner: NgxSpinnerService,
-                      public dialog: MatDialog  ) { }
+              public dialog: MatDialog,
+              private utilisateurService: UtilisateurService,
+              private spinner: NgxSpinnerService) { }
 
   ngOnInit(): void {
     this.idUser = this.route.snapshot.paramMap.get('id');
@@ -34,41 +37,34 @@ export class UtilisateurdetailComponent implements OnInit {
   }
 
   getUtilisateurByIdUser(idUser:String){
-    this.spinner.show();
+   // this.spinner.show();
     this.utilisateurService.getUtilisateurByIdUser(idUser)
         .subscribe(utilisateur => {
             this.utilisateurSelected = utilisateur as Utilisateur;
-            this.spinner.hide();
+         //   this.spinner.hide();
         })
   }
 
-  onUpdate(){
-    this.spinner.show();
-    this.utilisateurService.updateUtilisateurByIdUser(this.idUser, this.utilisateurSelected)
-        .subscribe(utilisateur => {
-            this.utilisateurSelected = utilisateur as Utilisateur
-            console.log("utilisateur => " + this.utilisateurSelected);
-            this.spinner.hide();
-            this.router.navigate(["/Utilisateur"]);
 
-        })
-  }
+    openDialog(type:Number): void {
 
-  onDelete(){
-    this.spinner.show();
-    this.utilisateurService.deleteUtilisateurByIdUser(this.idUser)
-        .subscribe(() => {
-            console.log("utilisateur deleted");
-            this.spinner.hide();
-            this.router.navigate(["/Utilisateur"]);
-        })
-  }
-    openDialog(): void {
+        switch(type){
+            case 0:
+                this.titre = "Modification";
+                this.description = "Êtes-vous sur de modifier "+ this.utilisateurSelected.nom +" " + this.utilisateurSelected.prenom + "?";
+                break;
+            case 1:
+                this.titre = "Suppression",
+                this.description = "Êtes-vous sur de supprimer "+ this.utilisateurSelected.nom +" " + this.utilisateurSelected.prenom + "?";
+                break;
+            default:
+                console.log("default opération");
+        }
+
         const dialogRef = this.dialog.open(DialogOverviewExampleDialog, {
-          width: '250px',
-          data: {titre: "Delete", description: "Êtes-vous sur de modifier?", utilisateurData: this.utilisateurSelected}
-        });
-
+                             width: '250px',
+                             data: {titre: this.titre, description: this.description, idUser: this.idUser, utilisateurData: this.utilisateurSelected, typeOperation: type}
+                         });
       }
 }
 
@@ -79,15 +75,56 @@ export class UtilisateurdetailComponent implements OnInit {
 export class DialogOverviewExampleDialog {
   constructor(
     public dialogRef: MatDialogRef<DialogOverviewExampleDialog>,
-    @Inject(MAT_DIALOG_DATA) public data: DialogData) {}
+    @Inject(MAT_DIALOG_DATA) public data: DialogData,
+    private spinner: NgxSpinnerService,
+    private router: Router,
+    private utilisateurService: UtilisateurService,) {}
 
   onNoClick(): void {
     this.dialogRef.close();
   }
 
   // parametre number 0 = update et 1 = deleted;
-  onAction() : void {
-          console.log("ok pour action => " + (this.data.utilisateurData as Utilisateur).prenom);
+  onAction(type:Number) : void {
+    //this.spinner.show();
+    switch(type){
+        case 0:
+            console.log('update');
+            this.onUpdate(this.data.idUser, this.data.utilisateurData as Utilisateur)
+            break;
+        case 1:
+            console.log('delete');
+            this.onDelete(this.data.idUser);
+            break;
+        default:
+            console.log('default modal');
+    }
+    //console.log("ok pour action => " + (this.data.utilisateurData as Utilisateur).prenom);
   }
+
+  onUpdate(idUser:String,  utilisateur: Utilisateur){
+      console.log("iduser: " + idUser + " utilisateur name: " + utilisateur.nom);
+      this.spinner.show();
+      this.utilisateurService.updateUtilisateurByIdUser(idUser, utilisateur)
+          .subscribe(utilisateur => {
+              //this.utilisateurSelected = utilisateur as Utilisateur
+             // console.log("utilisateur => " + this.utilisateurSelected);
+             this.spinner.hide();
+             this.dialogRef.close();
+             this.router.navigate(["/Utilisateur"]);
+
+          })
+    }
+
+    onDelete(idUser:String){
+      this.spinner.show();
+      this.utilisateurService.deleteUtilisateurByIdUser(idUser)
+          .subscribe(() => {
+              console.log("utilisateur deleted");
+              this.dialogRef.close();
+              this.spinner.hide();
+              this.router.navigate(["/Utilisateur"]);
+          })
+    }
 
 }
