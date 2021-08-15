1b7d5862d1489dfc803abd4da8700686b569bf45
@mevaElodie <rrmeva@yahoo.com>
2021-08-15 08:02:21 +0300

    Add affichage dasbord

--- src/app/page/dashboard/dashboard/dashboard.component.ts
+++ src/app/page/dashboard/dashboard/dashboard.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NgxSpinnerService } from "ngx-spinner";
+import { GoogleChartComponent } from 'angular-google-charts'; 
 
 @Component({
   selector: 'app-dashboard',
@@ -7,7 +9,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+  title = 'googlechart';  
+  type = 'AreaChart';  
+  data = [  
+     ['Name1', 5.0],  
+     ['Name2', 36.8],  
+     ['Name3', 42.8],  
+     ['Name4', 18.5],  
+     ['Name5', 16.2]  
+  ];  
+  //columnNames = ['Name', 'Percentage'];  
+  options = {      
+  };  
+  width = 500;  
+  height = 300;
+  constructor(private spinner: NgxSpinnerService) { }
 
   ngOnInit(): void {
   }
